1bf242fa26104e1fdcf3a0818848cdd05db1aaa5
MegatronKing <1256980529@qq.com>
2019-04-01 10:49:01 +0800

    Fixed status bar icon bugs

--- netbare-sample/src/main/kotlin/com/github/megatronking/netbare/sample/AppService.kt
+++ netbare-sample/src/main/kotlin/com/github/megatronking/netbare/sample/AppService.kt
@@ -42,7 +42,7 @@ class AppService : NetBareService() {
         val pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_UPDATE_CURRENT)
         return NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setSmallIcon(R.drawable.netbare_notification)
                 .setContentTitle(getString(R.string.app_name))
                 .setContentText(getString(R.string.app_name))
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
